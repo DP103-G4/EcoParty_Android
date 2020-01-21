50f693a14fac5d6e0cdad10b31a46b08a1aa5311
diehawk951 <diehawk951@gmail.com>
2020-01-21 15:37:13 +0800

    branchTest

--- app/src/main/java/tw/dp103g4/main_android/MainActivity.java
+++ app/src/main/java/tw/dp103g4/main_android/MainActivity.java
@@ -46,20 +46,6 @@ public class MainActivity extends AppCompatActivity {
         return bottomNavigationView;
     }
 
-//    // 重寫onTouchEvent的監聽方法
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        InputMethodManager imm= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-//            if (MainActivity.this.getCurrentFocus() != null) {
-//                if (MainActivity.this.getCurrentFocus().getWindowToken() != null) {
-//                    imm.hideSoftInputFromWindow(MainActivity.this.getCurrentFocus().getWindowToken(),
-//                            InputMethodManager.HIDE_NOT_ALWAYS);
-//                }
-//            }
-//        }
-//        return super.onTouchEvent(event);
-//    }
 
 
 }
